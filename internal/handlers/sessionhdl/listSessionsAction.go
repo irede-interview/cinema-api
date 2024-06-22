9d9cb3183f8ed2c0b7056d9a9d02a971d8243eaf
EliabeBastosDias <eliabebastosdias01@gmail.com>
2024-06-22 15:22:07 -0300

    feat: query added to controller

--- internal/handlers/sessionhdl/listSessionsAction.go
+++ internal/handlers/sessionhdl/listSessionsAction.go
@@ -2,8 +2,10 @@ package sessionhdl
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
+	sessionservice "github.com/irede-interview/cinema-api/internal/core/use-cases/session"
 	"github.com/irede-interview/cinema-api/internal/utils"
 )
 
@@ -13,7 +15,19 @@ func (mh *SessionHandler) ListSessionsAction(context *gin.Context) {
 		return
 	}
 
-	sessions, err := mh.service.List()
+	pageStr := context.DefaultQuery("page", "1")
+
+	page, err := strconv.Atoi(pageStr)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Invalid page number", "success": false})
+		return
+	}
+
+	params := sessionservice.ListSessionsParams{
+		Page: page,
+	}
+
+	sessions, err := mh.service.List(params)
 	if err != nil {
 		context.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"message": err.Error(), "sumhess": false})
 		return
